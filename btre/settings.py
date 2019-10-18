aa0881d1b6de9f1bfaa55a94a96455a620643511
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2019-10-18 16:30:10 +0100

    Inquiry check and send email functionality

--- btre/settings.py
+++ btre/settings.py
@@ -143,3 +143,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
